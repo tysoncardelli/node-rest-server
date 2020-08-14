ecdd0fa3884ab8923fb3804aeb53425e503cdda5
Tyson Cardelli <tysoncardelli@gmail.com>
2020-08-14 15:10:07 +0100

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -7,7 +7,7 @@ let urlDB;
 if(process.env.NODE_ENV == 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB= 'mongodb+srv://admin:bGO1cqhMxDO3Cwxd@cluster0.w7jkk.mongodb.net/cafe'
+    urlDB=  process.env.MONGO_URI
 }
 
 
